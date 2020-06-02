75f56c64054ae56fb23e3125c4af16d839df069d
ainurikdav <ainurikdav@gmail.com>
2020-06-02 23:48:25 +0300

    finish base table

---
+++ src/core/Emitter.js
@@ -0,0 +1,45 @@
+export class Emitter {
+  constructor() {
+    this.listeners = {}
+    console.log(this.listeners)
+  }
+
+  // dispatch  уведомляем
+  // table.emit('table.select', {a:2})
+  emit(event, ...args) {
+    if (!Array.isArray(this.listeners[event])) {
+      return false
+    }
+    this.listeners[event].forEach((listener) => {
+      listener(...args)
+    })
+  }
+
+  // on, listen подписываемся на уведомления
+  // formula.subscribe('table:select', () => {})
+  subscribe(event, fn) {
+    this.listeners[event] = this.listeners[event] || []
+    this.listeners[event].push(fn)
+    return () => {
+      this.listeners[event] =
+        this.listeners[event].filter((listener) => listener !== fn)
+    }
+  }
+}
+
+// const emitter = new Emitter()
+//
+// const unsub = emitter.subscribe('Aynur', (data) => console.log(data))
+//
+// setTimeout(() => {
+//   emitter.emit('Aynur', '2000')
+// }, 2000)
+//
+// setTimeout(() => {
+//   unsub()
+// }, 3000)
+//
+// setTimeout(() => {
+//   emitter.emit('Aynur', '4000')
+// }, 4000)
+
--- src/core/dom.js
+++ src/core/dom.js
@@ -13,6 +13,17 @@ class Dom {
     return this.$el.outerHTML
   }
 
+  text(text) {
+    if (typeof text === 'string' ) {
+      this.$el.textContent = text
+      return this
+    }
+    if (this.$el.tagName.toLowerCase() === 'input') {
+      return this.$el.value.trim()
+    }
+    return this.$el.textContent.trim()
+  }
+
   clear() {
     this.html('')
     return this
@@ -43,28 +54,47 @@ class Dom {
     return $(this.$el.closest(selector))
   }
 
+  focus() {
+    this.$el.focus()
+    return this
+  }
+
   getCoords() {
     return this.$el.getBoundingClientRect()
+  }
+
+  id(parse) {
+    if (parse) {
+      const parsed = this.id().split(':')
+      return {
+        row: +parsed[0],
+        col: +parsed[1],
+      }
+    }
+    return this.data.id
   }
 
   get data() {
     return this.$el.dataset
   }
 
+  find(selector) {
+    return $(this.$el.querySelector(selector))
+  }
+
   findAll(selector) {
     return this.$el.querySelectorAll(selector)
   }
 
+  addClass(className) {
+    this.$el.classList.add(className)
+    return this
+  }
 
-  /*
-
-  {
-    height: '30px',
-    width: '42px',
-    backgroundColor: red
-    }
-
-  */
+  removeClass(className) {
+    this.$el.classList.remove(className)
+    return this
+  }
 
   css(styles = {}) {
     // РЕАЛИЗАЦИЯ ЗАДАЧИ ПРИ ПОМОЩИ ЦИКЛА FOR IN
